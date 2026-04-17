95dda608a97cdb4d1a39912b4ebb94895a3a688d
Elite <pro100elite@gmail.com>
2026-04-17 22:43:42 +0200

    Hp bar optimize, prefabs meta fix

--- src/Project2026/Assets/Code/Game/Features/Health/HpBarView.cs
+++ src/Project2026/Assets/Code/Game/Features/Health/HpBarView.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+using DG.Tweening;
 using UnityEngine;
 
 namespace Code.Game.Features.Health
@@ -9,33 +9,21 @@ namespace Code.Game.Features.Health
         [SerializeField] private Transform _backFill;
         [Space]
         [SerializeField] private float _fullWidth = 0.58f;
-        [SerializeField] private float _backDelaySpeed = 0.2f;
+        [SerializeField] private float _backDelaySpeed = 0.3f;
 
-        private float _backVelocity;
+        private Tween _backTween;
 
         public void SetHp(float currentPercent)
         {
             _fill.localScale = new Vector3(_fullWidth * currentPercent, _fill.localScale.y, _fill.localScale.z);
 
-            StopAllCoroutines();
-            StartCoroutine(AnimateBackFill(currentPercent));
+            _backTween?.Kill();
+            _backTween = _backFill.DOScaleX(_fullWidth * currentPercent, _backDelaySpeed).SetEase(Ease.OutSine);
         }
 
-        private IEnumerator AnimateBackFill(float targetPercent)
+        private void OnDestroy()
         {
-            while (Mathf.Abs(_backFill.localScale.x - _fullWidth * targetPercent) > 0.001f)
-            {
-                var newX = Mathf.SmoothDamp(
-                    _backFill.localScale.x,
-                    _fullWidth * targetPercent,
-                    ref _backVelocity,
-                    _backDelaySpeed
-                );
-
-                _backFill.localScale = new Vector3(newX, _backFill.localScale.y, _backFill.localScale.z);
-
-                yield return null;
-            }
+            _backTween?.Kill();
         }
     }
 }
