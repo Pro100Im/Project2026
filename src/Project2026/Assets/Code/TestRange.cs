247107726b8dc9e55b55e0e2218db0f343aa7ddc
Elite <pro100elite@gmail.com>
2026-05-14 23:16:04 +0200

    Minor fix, test range

---
+++ src/Project2026/Assets/Code/TestRange.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class TestRange : MonoBehaviour
+{
+    public float CellSize = 0.4f;
+    public float Range = 6f;
+
+    public bool ShowOnlySelected = true;
+
+    private void OnDrawGizmos()
+    {
+        if (ShowOnlySelected) 
+            return;
+
+        DrawRange();
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        DrawRange();
+    }
+
+    private void DrawRange()
+    {
+        Gizmos.color = new Color(1.2f, 0f, 0f, 0.6f);
+
+        Gizmos.DrawWireSphere(transform.position, Range * CellSize);
+
+        Gizmos.color = new Color(1.2f, 0f, 0f, 0.2f);
+        DrawFlatDisk(transform.position, Range * CellSize);
+    }
+
+    private void DrawFlatDisk(Vector3 center, float radius)
+    {
+        var oldMatrix = Gizmos.matrix;
+
+        Gizmos.matrix = Matrix4x4.TRS(center, Quaternion.identity, new Vector3(1, 0.01f, 1));
+        Gizmos.DrawSphere(Vector3.zero, radius);
+        Gizmos.matrix = oldMatrix;
+    }
+}
